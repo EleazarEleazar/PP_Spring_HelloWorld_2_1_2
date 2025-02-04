1cda85096808fdc821259a6cf1c0452667a1e0ae
Elizar <elizarelayzo@gmail.com>
2025-02-04 22:07:53 +0300

    first try

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,28 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
+
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("\"HelloWorld\" beans are the same? - " + (bean1 == bean2));
+        System.out.println("\"Cat\" beans are the same? - " + (cat1 == cat2));
+
+
+
+
     }
+
+
 }
